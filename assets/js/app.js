a027743cafcb9bad56d766872156ed65b6899af2
Tayyab Ferozi <ferozitayyab@gmail.com>
2021-10-02 01:06:28 +0500

    fix responsiveness issues

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,40 +7,49 @@ $("#slick").slick({
     '<button type="button" class="slick-next"><img src="./assets/vectors/right-arrow-slider.svg" /></button>',
   prevArrow:
     '<button type="button" class="slick-prev"><img src="./assets/vectors/left-arrow-slider.svg" /></button>',
+  dots: true,
   responsive: [
     {
       breakpoint: 1068,
       settings: {
         arrows: false,
         centerMode: true,
-        centerPadding: "0px",
-        slidesToShow: 3,
-      },
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: "40px",
-        slidesToShow: 3,
-      },
-    },
-    {
-      breakpoint: 650,
-      settings: {
-        arrows: false,
-        centerMode: true,
         centerPadding: "60px",
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 690,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "190px",
         slidesToShow: 1,
       },
     },
     {
-      breakpoint: 650,
+      breakpoint: 610,
       settings: {
         arrows: false,
         centerMode: true,
-        centerPadding: "0px",
+        centerPadding: "150px",
+        slidesToShow: 1,
+      },
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "100px",
+        slidesToShow: 1,
+      },
+    },
+    {
+      breakpoint: 410,
+      settings: {
+        arrows: false,
+        centerMode: true,
         slidesToShow: 1,
       },
     },
--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,108 +1,33 @@
-// vars
-"use strict";
-var testim = document.getElementById("testim"),
-  testimDots = Array.prototype.slice.call(
-    document.getElementById("testim-dots").children
-  ),
-  testimContent = Array.prototype.slice.call(
-    document.getElementById("testim-content").children
-  ),
-  testimLeftArrow = document.getElementById("left-arrow"),
-  testimRightArrow = document.getElementById("right-arrow"),
-  testimSpeed = 4500,
-  currentSlide = 0,
-  currentActive = 0,
-  testimTimer,
-  touchStartPos,
-  touchEndPos,
-  touchPosDiff,
-  ignoreTouch = 30;
-window.onload = function () {
-  // Testim Script
-  function playSlide(slide) {
-    for (var k = 0; k < testimDots.length; k++) {
-      testimContent[k].classList.remove("active");
-      testimContent[k].classList.remove("inactive");
-      testimDots[k].classList.remove("active");
-    }
+const wrapper = document.querySelector(".wrapper");
+const indicators = [...document.querySelectorAll(".indicators button")];
 
-    if (slide < 0) {
-      slide = currentSlide = testimContent.length - 1;
-    }
+let currentTestimonial = 0; // Default 0
+let currentActive = 0;
 
-    if (slide > testimContent.length - 1) {
-      slide = currentSlide = 0;
-    }
+indicators.forEach((item, i) => {
+  item.addEventListener("click", () => {
+    $(".indicators button").removeClass("active");
+    wrapper.style.marginLeft = `-${100 * i}%`;
+    currentActive = i;
+    item.classList.add("active");
+    currentTestimonial = i;
+  });
+});
 
-    if (currentActive != currentSlide) {
-      testimContent[currentActive].classList.add("inactive");
-    }
-    testimContent[slide].classList.add("active");
-    testimDots[slide].classList.add("active");
-
-    currentActive = currentSlide;
-
-    clearTimeout(testimTimer);
-    testimTimer = setTimeout(function () {
-      playSlide((currentSlide += 1));
-    }, testimSpeed);
+$(".btn-next").click(function () {
+  $(".indicators button").removeClass("active");
+  if (currentActive !== indicators.length - 1) {
+    currentActive += 1;
   }
+  wrapper.style.marginLeft = `-${100 * currentActive}%`;
+  $(`.indicators button:eq(${currentActive})`).addClass("active");
+});
 
-  testimLeftArrow.addEventListener("click", function () {
-    playSlide((currentSlide -= 1));
-  });
-
-  testimRightArrow.addEventListener("click", function () {
-    playSlide((currentSlide += 1));
-  });
-
-  for (var l = 0; l < testimDots.length; l++) {
-    testimDots[l].addEventListener("click", function () {
-      playSlide((currentSlide = testimDots.indexOf(this)));
-    });
+$(".btn-prev").click(function () {
+  $(".indicators button").removeClass("active");
+  if (currentActive !== 0) {
+    currentActive -= 1;
   }
-
-  playSlide(currentSlide);
-
-  // keyboard shortcuts
-  document.addEventListener("keyup", function (e) {
-    switch (e.keyCode) {
-      case 37:
-        testimLeftArrow.click();
-        break;
-
-      case 39:
-        testimRightArrow.click();
-        break;
-
-      case 39:
-        testimRightArrow.click();
-        break;
-
-      default:
-        break;
-    }
-  });
-
-  testim.addEventListener("touchstart", function (e) {
-    touchStartPos = e.changedTouches[0].clientX;
-  });
-
-  testim.addEventListener("touchend", function (e) {
-    touchEndPos = e.changedTouches[0].clientX;
-
-    touchPosDiff = touchStartPos - touchEndPos;
-
-    console.log(touchPosDiff);
-    console.log(touchStartPos);
-    console.log(touchEndPos);
-
-    if (touchPosDiff > 0 + ignoreTouch) {
-      testimLeftArrow.click();
-    } else if (touchPosDiff < 0 - ignoreTouch) {
-      testimRightArrow.click();
-    } else {
-      return;
-    }
-  });
-};
+  wrapper.style.marginLeft = `-${100 * currentActive}%`;
+  $(`.indicators button:eq(${currentActive})`).addClass("active");
+});
